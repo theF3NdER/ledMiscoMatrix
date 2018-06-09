dd575e6e79a6924bd4c5e14ab901f9122d66e611
Riccardo Ancona <riccardo.ancona@gmail.com>
2018-06-10 01:36:47 +0200

    Updated main.ino

--- FW/src/main/main.ino
+++ FW/src/main/main.ino
@@ -1,10 +1,10 @@
 #include <Arduino.h>
 #include <FastLED.h>
 
-#define NUM_LEDS 720
-#define NUM_STRIPS 8
+#define NUM_LEDS_PER_STRIPE 240
+#define NUM_STRIPS 3
 
-CRGB leds[NUM_LEDS];
+CRGB leds[NUM_LEDS_PER_STRIPE];
 CLEDController *controllers[NUM_STRIPS];
 uint8_t gBrightness = 128;
 
@@ -13,7 +13,6 @@ void setup()
   controllers[0] = &FastLED.addLeds<WS2812,1>(leds, NUM_LEDS);
   controllers[1] = &FastLED.addLeds<WS2812,2>(leds, NUM_LEDS);
   controllers[2] = &FastLED.addLeds<WS2812,10>(leds, NUM_LEDS);
-  controllers[3] = &FastLED.addLeds<WS2812,11>(leds, NUM_LEDS);
 }
 
 void loop() 
@@ -30,7 +29,4 @@ void loop()
   fill_solid(leds, NUM_LEDS, CRGB::Blue);
   controllers[2]->showLeds(gBrightness);
 
-  // draw led data for the first strand into leds
-  fill_solid(leds, NUM_LEDS, CRGB::White);
-  controllers[3]->showLeds(gBrightness);
 }
