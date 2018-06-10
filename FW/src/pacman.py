31b38b053ee18519a6548dd235da88f7db882514
Riccardo Ancona <riccardo.ancona@gmail.com>
2018-06-10 19:48:33 +0000

    Added all available pins in comment inside

--- FW/src/pacman.py
+++ FW/src/pacman.py
@@ -15,14 +15,22 @@ import numpy as np
 LED_COUNT_PER_STRIPE = 45
 STRIPES_NO = 4
 LED_COUNT      = LED_COUNT_PER_STRIPE * STRIPES_NO      # Number of LED pixels.
-LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+LED_PIN        = 21      # GPIO pin connected to the pixels (18 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 250     # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 150     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
  
+# Pin Funzionanti:
+
+# 13 con LED_CHANNEL 1
+# 18 con LED_CHANNEL 0
+# 19 con LED_CHANNEL 1
+# 21 con LED_CHANNEL 1
+
+
 def colorWipe(strip, color, wait_ms=50):
         """Wipe color across display a pixel at a time."""
         for i in range(strip.numPixels()):
@@ -38,7 +46,6 @@ def openFrame(file):
         for row in reader:
             aList.append(row)
         # i need to strip the extra white space from each string in the row
-        print(aList)
         return(aList)
 
 # Main program logic follows:
@@ -58,7 +65,7 @@ if __name__ == '__main__':
         print('Use "-c" argument to clear LEDs on exit')
  
     try:
-        FPS = 1
+        FPS = 30
         wait_ms = 1000.0/FPS
         colorvalue = 0
         color = Color(colorvalue,colorvalue,colorvalue)
@@ -73,19 +80,14 @@ if __name__ == '__main__':
 
             for i in range(strip.numPixels()):
                 r = i // LED_COUNT_PER_STRIPE
-                print("ROW " + str(r))
                 odd_row = r % 2
                 pos = i
                 if (odd_row == 1):
                     pos = int(LED_COUNT_PER_STRIPE * (r+1)) - 1 - i % LED_COUNT_PER_STRIPE
                 
-                print("POOOS: " + str(pos) )
-
-                color = Color(int(my_data[pos][0]),int(my_data[pos][1]),int(my_data[pos][2]))
+                color = Color(int(my_data[pos][2]),int(my_data[pos][1]),int(my_data[pos][0]))
                 strip.setPixelColor(i, color)
             
-            
-            print(index)
             
             strip.show()
             time.sleep(wait_ms/1000.0)
