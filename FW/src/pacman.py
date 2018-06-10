bb7428d95aee3e232030af82631f715aca100c08
Riccardo Ancona <riccardo.ancona@gmail.com>
2018-06-10 17:12:09 +0000

    Added FPS

--- FW/src/pacman.py
+++ FW/src/pacman.py
@@ -10,16 +10,23 @@ from neopixel import *
 import argparse
  
 # LED strip configuration:
-LED_COUNT      = 160      # Number of LED pixels.
+LED_COUNT      = 180      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 55     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
  
- 
+def colorWipe(strip, color, wait_ms=50):
+        """Wipe color across display a pixel at a time."""
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, color)
+            strip.show()
+            time.sleep(wait_ms/1000.0)
+
+
 # Main program logic follows:
 if __name__ == '__main__':
     # Process arguments
@@ -37,15 +44,21 @@ if __name__ == '__main__':
         print('Use "-c" argument to clear LEDs on exit')
  
     try:
-        wait_ms = 30
-        color = Color(127, 127, 127)
+        FPS = 30
+        wait_ms = 1/FPS
+        colorvalue = 0
+        color = Color(colorvalue,colorvalue,colorvalue)
+        
 
         while True:
+            color = Color(colorvalue,colorvalue,colorvalue)
             for i in range(strip.numPixels()):
                 strip.setPixelColor(i, color)
-                strip.show()
-                time.sleep(wait_ms/1000.0)
+            
+            strip.show()
+            colorvalue = (colorvalue + 1) % 255
+            time.sleep(wait_ms)
     
     except KeyboardInterrupt:
-        if args.clear:
-            colorWipe(strip, Color(0,0,0), 10)
+        #if args.clear:
+        colorWipe(strip, Color(0,0,0), 10)
