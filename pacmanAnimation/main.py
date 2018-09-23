01ed1e48f35abe2e8dff08fa9b2ff4d919eae4c0
Davide <gffdvd@gmail.com>
2018-09-23 09:55:27 +0200

    ready to september

--- pacmanAnimation/main.py
+++ pacmanAnimation/main.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import pickle
 
 def assembleCentralBlock(one, two, three):
     two = np.hstack((two, one))
@@ -22,31 +23,45 @@ def rightBlock(centralBlock, size):
     centralAndRight = np.hstack((centralBlock, (np.zeros((16, size, 3), np.uint8))))
     return centralAndRight
 
-def makeFullImage(ghost, pacman, canvas, w):
-    for i in range(w*2):
+def makeFullImage(ghost, pacman, w):
+    for i in range(w*3):
         centralBlock = assembleCentralBlock(ghost[i%2], ghost[i%2], pacman[i%4])
         asd = leftBlock(centralBlock, i)
         fullImage = rightBlock(asd, w*4-i)
         # extractCentralFrames(fullImage, '%03d'%i+'.txt', w)
-        extractCentralFrames(fullImage, 'frames/4/'+'%03d'%i+'.txt', w)
+        extractCentralFrames(fullImage, 'frames/4/'+'%03d'%i+'.txt', 'frames/4/'+'%03d'%i+'.png', w)
     
 
-def extractCentralFrames(fullImage, name, w):
-    croppedImage = fullImage[:4, w:w*2]
-    # croppedImage = fullImage[:, 60:120]
-    # cv2.imwrite(name, croppedImage)
-    # np.savetxt(name, fullImage)
-    fullImage.tofile(name, sep=" ",format="%s")
+def extractCentralFrames(fullImage, nameTxt, namePng, w):
+    # croppedImage = fullImage[7:11, w:w*2]
+    croppedImage = fullImage[:, 60:105]
+    cv2.imwrite(namePng, croppedImage)
+    # cv2.imshow("pacman", fullImage)
+    # np.savetxt(nameTxt, fullImage)
+    # fullImage.tofile(nameTxt, sep=" ",format="%s")
+
+    # output = open(name, 'wb')
+    # pickle.dump(fullImage, output)
+    # output.close()
+
+    with open(nameTxt, "w") as output:
+        for c in xrange(16):
+            for r in xrange(w):
+                string = ','.join(map(str, croppedImage[c][r]))+"\n"
+                output.write(string)
 
 
 def main():
     w = 45
-    h = 16
-    canvas = np.zeros((h, w*3, 3), np.uint8)
 
     ghost, pacman = loadSprites()
 
-    makeFullImage(ghost, pacman, canvas, w)
+    # while True:
+    makeFullImage(ghost, pacman, w)
+    
+    cv2.destroyAllWindows()
+
+
 
 if __name__ == "__main__":
     main()
